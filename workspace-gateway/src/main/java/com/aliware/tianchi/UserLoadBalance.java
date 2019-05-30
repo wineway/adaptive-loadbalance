285403082c20235e32e9ae1af83a1b33b1e5faa1
Yuwen Wang <ywang1@hetao.ai>
2019-05-30 21:03:40 +0800

    test

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -1,10 +1,12 @@
 package com.aliware.tianchi;
 
 import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.extension.ExtensionLoader;
 import org.apache.dubbo.rpc.Invocation;
 import org.apache.dubbo.rpc.Invoker;
 import org.apache.dubbo.rpc.RpcException;
 import org.apache.dubbo.rpc.cluster.LoadBalance;
+import org.apache.dubbo.rpc.listener.CallbackListener;
 
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
@@ -21,6 +23,10 @@ public class UserLoadBalance implements LoadBalance {
 
     @Override
     public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
+        ExtensionLoader<CallbackListener> extensionLoader = ExtensionLoader.getExtensionLoader(CallbackListener.class);
+        CallbackListener defaultExtension = extensionLoader.getExtension("callbacklistenerimpl");
+        defaultExtension.receiveServerMsg("-------hello---------");
         return invokers.get(ThreadLocalRandom.current().nextInt(invokers.size()));
     }
+
 }
